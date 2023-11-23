b345e703e06f7b5cab0a41ba57eb5469d0726a7e
MMXVIII404 <han1999@outlook.com>
2023-11-23 05:16:16 +0000

    Added catch mini game v1

--- Assets/Scripts/AimOnTarget.cs
+++ Assets/Scripts/AimOnTarget.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,9 +12,17 @@ public class AinOnTarget : MonoBehaviour
     public float checkRange = 3f;
     public GameObject displayText;
 
+    public Image checkArea;
+
     bool buttonPressed = false;
     bool inRange = false;
     string currentMonsterName = string.Empty;
+
+    float timeCount = 0;
+    bool startCountTime = false;
+    bool catched = false;
+    bool startCatching = false;
+
     //public Button catchButton;
 
     void Start()
@@ -22,10 +32,46 @@ public class AinOnTarget : MonoBehaviour
 
     void Update()
     {
-        Catch();
+        //Catch();
+        CountTime();
+        PointAtMonster();
     }
 
-    private void Catch()
+
+    public void SetCatchStatus()
+    {
+        if (inRange)
+        {
+            buttonPressed = true;
+        }
+    }
+
+
+    private bool IsPointInsideCheckArea(GameObject Monster)
+    {
+        Vector2 checkAreaPostion = RectTransformUtility.WorldToScreenPoint(mainCamera, Monster.transform.position);    // Monster postion on screen
+        checkArea.GetComponent<RectTransform>().position = checkAreaPostion;   // Check area follow the Monster position on screen
+
+        float distanceToCheckArea = Vector2.Distance(checkAreaPostion, new Vector2(Screen.width / 2, Screen.height / 2));
+
+        float radius = checkArea.rectTransform.sizeDelta.x * 0.5f;
+
+        return distanceToCheckArea <= radius; ;
+    }
+
+    void CountTime()
+    {
+        if (startCountTime)
+        {
+            timeCount += Time.deltaTime;
+        }
+        else
+        {
+            timeCount = 0;
+        }
+    }
+
+    void PointAtMonster()
     {
         Ray ray = mainCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
         RaycastHit hit;
@@ -34,47 +80,98 @@ public class AinOnTarget : MonoBehaviour
         {
             if (hit.collider.CompareTag("Monster"))
             {
-
                 float distanceToMonster = Vector3.Distance(mainCamera.transform.position, hit.point);
-                // Debug.Log(distanceToMonster);
-                if (distanceToMonster > checkRange)     // ׼��Ϊ�ʺ�
-                {
-                    displayText.GetComponent<TMP_Text>().text = "?";
-                    displayText.GetComponent<TMP_Text>().color = Color.red;
-                    // Debug.Log("Too far!");
-                }
-                else if (distanceToMonster <= checkRange)    // ׼��Ϊ��׽��
-                {
-                    displayText.GetComponent<TMP_Text>().text = "Scanning...";
-                    inRange = true;
-                    displayText.GetComponent<TMP_Text>().color = Color.yellow;
 
-                    if (buttonPressed)
+                if (distanceToMonster <= checkRange)    // Can catch the Monster.
+                {
+                    if (!startCatching)
                     {
-                        // TODO: Add some VFX to destroy the monster
-
-                        currentMonsterName = hit.collider.gameObject.name;
-                        Destroy(hit.collider.gameObject);
-
-                        buttonPressed = false;
-                        inRange = false;
+                        displayText.GetComponent<TMP_Text>().text = "Can Catch!";
+                        displayText.GetComponent<TMP_Text>().color = Color.yellow;
                     }
+                    inRange = true;
 
-                    // Debug.Log("Can catch!");
+                    if ( buttonPressed && !startCatching)
+                    {
+                        startCatching = true;
+                        StartCoroutine(StartCatching(hit.collider.gameObject));
+                    }
                 }
-                else
+                else    // Too far away from Monster.
                 {
-                    displayText.GetComponent<TMP_Text>().text = "No Monster";
-                    // Debug.Log("Nothing!");
+                    displayText.GetComponent<TMP_Text>().text = "Too Far!";
+                    displayText.GetComponent<TMP_Text>().color = Color.red;
+                    inRange = false;
+                    startCatching = false;
                 }
             }
+            else    // Not point at a Monster.
+            {
+                displayText.GetComponent<TMP_Text>().text = "No Monster!";
+                displayText.GetComponent<TMP_Text>().color = Color.blue;
+                inRange = false;
+                startCatching = false;
+            }
+        }
+        else
+        {
+            displayText.GetComponent<TMP_Text>().text = "Search Around!";
+            displayText.GetComponent<TMP_Text>().color = Color.gray;
+            inRange = false;
+            startCatching = false; // 重置捕捉状态
         }
     }
-    public void SetCatchStatus(bool status)
+
+    IEnumerator StartCatching(GameObject Monster)
     {
-        if (inRange)
+        buttonPressed = false;
+        // 显示扫描信息并等待两秒
+        displayText.GetComponent<TMP_Text>().text = "Prepare to scan...";
+        displayText.GetComponent<TMP_Text>().color= Color.green;
+        yield return new WaitForSeconds(2f); // 等待2秒
+
+        // 激活扫描区域
+        checkArea.gameObject.SetActive(true);
+        startCountTime = true;
+
+        float stayTime = 0f;
+
+        while (startCatching) // 使用 startCatching 来控制循环
         {
-            buttonPressed = status;
+            if (IsPointInsideCheckArea(Monster))
+            {
+                displayText.GetComponent<TMP_Text>().text = $"Scanning... {stayTime:F1}";
+                stayTime += Time.deltaTime;
+
+                if (stayTime >= 5f)
+                {
+                    displayText.GetComponent<TMP_Text>().text = "Monster caught!";
+                    checkArea.gameObject.SetActive(false);
+                    yield return new WaitForSeconds(2f); // 等待2秒
+                    // TODO: Add some VFX to destroy the monster
+                    Destroy(Monster); // 假设您要销毁怪物对象
+                    break; // 成功捕捉，退出循环
+                }
+            }
+            else
+            {
+                stayTime = 0f; // 如果怪物移出了检测区域，重置计时器
+                displayText.GetComponent<TMP_Text>().text = "Monster escaped!";
+                checkArea.gameObject.SetActive(false);
+                yield return new WaitForSeconds(2f); // 等待2秒
+
+                // 可以在这里添加逻辑，如果怪物逃走了，是否停止捕捉
+                startCatching = false;
+
+                break;
+            }
+            yield return null;
+
+            Debug.Log(stayTime);
         }
+
+        
+        startCountTime = false;
+        startCatching = false;
     }
 }
