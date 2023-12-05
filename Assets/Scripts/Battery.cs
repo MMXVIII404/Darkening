b258c86a58cf4bc23d89f1f41418c294cc325ca7
MMXVIII404 <han1999@outlook.com>
2023-12-05 19:24:18 +0000

    Fixed Battery decrease and Monster face camera

--- Assets/Scripts/Battery.cs
+++ Assets/Scripts/Battery.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Battery : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Slider batterySlider;
+    private float decreaseRatePerSecond;
+    public Image fill;
+
     void Start()
     {
-        
+        decreaseRatePerSecond = 1 / 300f; // 在5分钟内减至0'
+        fill.color = Color.green;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (batterySlider.value > 0)
+        {
+            batterySlider.value -= decreaseRatePerSecond * Time.deltaTime;
+        }
+        if (batterySlider.value < 0.2)
+        {
+            fill.color = Color.red;
+        }
+    }
+
+    public void UseBattery(float usage)
+    {
+        batterySlider.value -= usage;
     }
 }
